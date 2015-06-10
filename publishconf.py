318b5a6ef3ac7f4cf3e0a44f5bf89bef6bfe73b7
Amit Saha <amitsaha.in@gmail.com>
2015-06-10 12:46:02 +1000

    Fedora 22 scientific post

--- publishconf.py
+++ publishconf.py
@@ -10,10 +10,10 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://amitsaha.github.io'
-RELATIVE_URLS = False
+#SITEURL = 'http://amitsaha.github.io'
+#RELATIVE_URLS = True
 
-FEED_DOMAIN = SITEURL
+#FEED_DOMAIN = SITEURL
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
 
