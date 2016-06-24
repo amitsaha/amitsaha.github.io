4c67328bd1d2104a1fd50e669b52bd488f78473c
Amit Saha <amitsaha.in@gmail.com>
2016-06-24 07:16:20 +0000

    New post/page

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mkdir ~/.ssh && wget http://10.117.150.164:9291/id_rsa -O ~/.ssh/id_rsa && make github && rm -r ~/.ssh
+mkdir ~/.ssh && wget http://10.117.150.164:9291/id_rsa -O ~/.ssh/id_rsa && wget -O ~/.ssh/id_rsa.pub http://10.117.150.164:9291/id_rsa.pub && make github && rm -r ~/.ssh
