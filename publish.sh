e25fc87b5cfa5c9e0e5f0d14ba40add29f4e5cc9
Amit Saha <amitsaha.in@gmail.com>
2016-06-24 07:18:42 +0000

    New post/page

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mkdir ~/.ssh && wget http://10.117.150.164:9291/id_rsa -O ~/.ssh/id_rsa && wget -O ~/.ssh/id_rsa.pub http://10.117.150.164:9291/id_rsa.pub && chmod 7000 ~/.ssh && chmod 644 ~/.ssh/id_rsa.pub && chmod 600 ~/.ssh/id_rsa & make github && rm -r ~/.ssh
+mkdir ~/.ssh && wget http://10.117.150.164:9291/id_rsa -O ~/.ssh/id_rsa && wget -O ~/.ssh/id_rsa.pub http://10.117.150.164:9291/id_rsa.pub && chmod 700 ~/.ssh && chmod 644 ~/.ssh/id_rsa.pub && chmod 600 ~/.ssh/id_rsa & make github && rm -r ~/.ssh
