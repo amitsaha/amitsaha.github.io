74d55ee805ca83568cf977f391258ee652e2437c
Amit Saha <amitsaha.in@gmail.com>
2016-07-01 11:07:25 +1000

    New post/page

--- publish.sh
+++ publish.sh
@@ -4,7 +4,7 @@ docker build \
     --build-arg git_username="Amit Saha" \
     --build-arg git_useremail="amitsaha.in@gmail.com" \
     --build-arg gid=`id -g` \
-    --build-arg group=`id --group` \
+    --build-arg group=`id --group --name` \
     --build-arg uid=`id --user` \
     --build-arg user=`id -u -n` \
     -t amitsaha/pelican  .
