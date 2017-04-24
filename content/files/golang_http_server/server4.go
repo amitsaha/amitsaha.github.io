c36aeb23d1ed094ca198b0e24f823f681aedf4b9
Amit Saha <amitsaha.in@gmail.com>
2017-04-24 17:56:49 +1000

    update

--- content/files/golang_http_server/server4.go
+++ content/files/golang_http_server/server4.go
@@ -16,16 +16,15 @@ func StatusHandler(w http.ResponseWriter, r *http.Request) {
 
 func RunSomeCode(handler http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        log.Printf("Got a %s request for: %v", r.Method, r.URL)
+		log.Printf("Got a %s request for: %v", r.Method, r.URL)
 		handler.ServeHTTP(w, r)
-        // At this stage, our handler has "handled" the request
-        // but we can still write to the client there
-        // but we won't do that
-        // XXX: We don't have the HTTP status here either, need to understand this better why
-        log.Println("Handler finished processing request")
-		})
+		// At this stage, our handler has "handled" the request
+		// but we can still write to the client there
+		// but we won't do that
+		// XXX: We don't have the HTTP status here either, need to understand this better why
+		log.Println("Handler finished processing request")
+	})
 }
-
 
 func main() {
 
