5ff0b782700ea47bb12f80245f6f01d7fe8f9244
Amit Saha <amitsaha.in@gmail.com>
2019-06-15 15:40:29 +1000

    update

--- pelican2hugo/insertcode.py
+++ pelican2hugo/insertcode.py
@@ -3,14 +3,14 @@ with open(sys.argv[1]) as f:
     newlines = ""
     lines = f.readlines()
     for line in lines:
-        if not line.startswith('.. code-include::'):
-            newlines += line
-        else:
+        if line.startswith('.. code-include::'):
             filepath = line.split('::')[1].lstrip().rstrip("\n")
             filedata = open(filepath).readlines()
-
             newlines += ".. code::\n\n"
-            for line in filedata:
-                newlines += "   " + line
-
+            for l in filedata:
+                newlines += "   " + l
+        elif ':lexer:' in line.strip():
+            continue
+        else:
+            newlines += line
 print(newlines)
